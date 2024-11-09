2926f2b1d289d14921349fcc56b49a312f8451a0
farhan khan <farhankhanpro3@gmail.com>
2024-11-09 22:28:45 +0530

    add toast

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { flushSync } from 'react-dom';
 import { createRoot } from 'react-dom/client';
+import toast from 'react-hot-toast';
+import { Toaster} from 'react-hot-toast';
 
 const Cards = ({ children }) => {
     function getCode(){
@@ -14,15 +16,19 @@ const Cards = ({ children }) => {
         .then(()=>{
             console.log("copied!")
         })
+        toast.success("Copied!")
         return code;
     }
  
   return (
+  <>
+    <Toaster/>
     <div className="w-60 h-72 bg-white hover:drop-shadow-2xl rounded-lg border border-gray-200 flex justify-center items-center
-    
+    cursor-pointer
     " onClick={getCode}>
       {children}
     </div>
+  </>
   );
 };
 
